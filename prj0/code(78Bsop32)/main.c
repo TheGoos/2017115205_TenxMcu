e56f13fbb84ebcf5723c941cda547bf01f818464
TheGoos <1248809529@qq.com>
2019-11-29 18:43:04 +0800

    《实验3 单端口复用控制LED》 编写实现代码，未能达到实验结果。但用Keil进行debug调试，逐一单步执行，基本接近实验结果。 （个人觉得，在某些指令前(后)还要添加延时）

--- prj0/code(78Bsop32)/main.c
+++ prj0/code(78Bsop32)/main.c
@@ -5,6 +5,7 @@ void main() {
   SysInit();
   VarsInit();
 	
+	modeValue = 1; //初始值为1
 	while(1){
 		//业务代码
 		TimeProcess();
@@ -19,20 +20,28 @@ void main() {
 	
 	}
 //=============================================================================
-	void TaskSetting(){
-		/* LED闪烁
-		F_ledOn();
-		delayMs(500);
-		F_ledOff();
-		delayMs(500);
-		*/
+	void TaskSetting(){	
+	//**单端口复用控制LED亮灭**
+	if (modeValue == 1){//模式标志为1时，该模式为推挽输出
 		//获取按键信息
+		P1MODL = 0xa8;		//将模式置为上拉输入,感知按键是否按下，只要处理时间够短，不影响LED显示
 		GetKeys();
+		delayMs(100);		//此处延时为确保能成功获取按键
 		//LED状态转换
 		if(D_keyValue1 == keyValue){
-			F_ledNeg();
+			Mode_Neg();		//模式标志为取反
 		}
-	}
+		P1MODL = 0xaa;		//将模式置为推挽输出，使LED显示
+		//delayMs(1000);
+		} else {//否则，该模式为上拉输入
+			P1MODL = 0xa8;	
+			GetKeys();
+			if(D_keyValue1 == keyValue){
+				Mode_Neg();		
+			}
+		}
+		
+	} 
 //=============================================================================
 	void TaskProcess(){
 	
