152737b2fcc628b778d61994ae5297848ed477fd
TheGoos <1248809529@qq.com>
2019-11-29 18:08:18 +0800

    《实验3 单端口复用控制LED》 定义 modeValue 变量，标志 P1_0 端口模式；同时定义模式取反函数 Mode_Neg() 。

--- prj0/code(78Bsop32)/main.h
+++ prj0/code(78Bsop32)/main.h
@@ -6,13 +6,13 @@
 
 // Globle Var -----------------------------------------
 #ifdef __main_c
-
+uint8_t modeValue; //pin脚模式标志
 #else
-
+extern uint8_t modeValue;
 #endif
 
 // Action Macro: exp: #define F_getData() ------
-
+#define Mode_Neg() modeValue = ~modeValue		//模式标志取反
 // Function ------------------------------------
 void delayMs(uint16_t time);
 void TimeProcess();
