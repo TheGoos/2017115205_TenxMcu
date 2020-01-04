4207292e80ed51e02c4ed73f3bd6d5446da56b8f
TheGoos <1248809529@qq.com>
2020-01-04 19:19:26 +0800

    《热水壶实验》 使数码管显示；存在问题：显示时不稳定。

--- prj0/code(78Bsop32)/display.c
+++ prj0/code(78Bsop32)/display.c
@@ -1,2 +1,54 @@
 #define __display_c
 #include "includeAll.h"
+
+code const uint8_t displayFonts[19] = {0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f,0x77,0x7c,0x39,0x5e,0x79,0x71,0x80,0x00,0xff};
+
+void SMG_Select(uint8_t position, uint8_t number)
+{
+    //选择数码管的位置
+    //根据电路图，电平为低时该位被选中
+    if (position == 1)
+    {
+        SMG_COM1 = 0;
+        SMG_COM2 = 1;
+    }
+    else if (position == 2)
+    {
+        SMG_COM1 = 1;
+        SMG_COM2 = 0;
+    }
+
+    //数位赋值
+    SMG_A = 0x01 & (displayFonts[number] >> 0);
+    SMG_B = 0x01 & (displayFonts[number] >> 1);
+    SMG_C = 0x01 & (displayFonts[number] >> 2);
+    SMG_D = 0x01 & (displayFonts[number] >> 3);
+    SMG_E = 0x01 & (displayFonts[number] >> 4);
+    SMG_F = 0x01 & (displayFonts[number] >> 5);
+    SMG_G = 0x01 & (displayFonts[number] >> 6);
+    SMG_DG = 0x01 & (displayFonts[number] >> 7);
+}
+
+void SMG_Display(uint8_t number)
+{
+		uint8_t i,j;
+		
+		if(number>100)
+			return;
+		//个位
+		j = number % 10;
+		SMG_Select(2, j);
+		delayMs(10);
+		//十位
+		i = (number - j)/10;
+		SMG_Select(1, i);
+		delayMs(10);
+}
+
+void Set_Pin_Sta()
+{
+		P1_2 = 1;
+		P1_7 = 1;
+}
+
+
--- prj0/code(78Bsop32)/display.h
+++ prj0/code(78Bsop32)/display.h
@@ -1,8 +1,22 @@
 #ifndef __display_h
 #define __display_h
 // Hal: exp: #define P_led P10 -----------------
-//#define P_led P1_1	
-#define P_led P1_0
+#define P_ledcom P1_0	
+#define P_led2 P1_2
+#define P_led1 P1_4
+
+#define SMG_COM1 P1_1
+#define SMG_COM2 P1_6
+
+#define SMG_A P1_4
+#define SMG_B P3_4
+#define SMG_C P3_5
+#define SMG_D P1_7
+#define SMG_E P3_6
+#define SMG_F P1_2
+#define SMG_G P3_2
+#define SMG_DG P1_3
+
 // Const: exp: #define D_data 1 ----------------
 #define D_ledOn 1  //高电位亮
 #define D_ledOff 0 //低点位灭
@@ -18,5 +32,7 @@
 #define F_ledOff() P_led = D_ledOff
 #define F_ledNeg() P_led = ~P_led
 // Function ------------------------------------
-
+void SMG_Select(uint8_t position, uint8_t number);
+void SMG_Display(uint8_t number);
+void Set_Pin_Sta();
 #endif
